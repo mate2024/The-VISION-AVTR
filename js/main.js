e3c2d56ac216d7c8cb26ffa12d1b2752dbda46d9
mate2024 <mate.natroshvili.2016@wp.pl>
2024-04-04 00:10:28 +0200

    aded video section

--- js/main.js
+++ js/main.js
@@ -1,3 +1,3 @@
-$(function(){
-
-});
+function play1(){
+    <iframe width="1200" height="600" src="https://www.youtube.com/embed/ChqM3zqTREQ?si=VVtXTPchWF9B0GzB" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+}
